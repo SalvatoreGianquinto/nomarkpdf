d75a557a89b32503e0d91dc94cc9dea41e3808e0
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2026-09-20 23:16:25 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="it">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
