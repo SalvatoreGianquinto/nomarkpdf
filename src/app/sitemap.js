da520874af6d88a5e85e5f693011ef8248d183b0
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2026-09-22 23:36:09 +0200

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://nomarklab.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nomarklab.app/pdf',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nomarklab.app/password',
+      lastModified: new Date(),
+    },
+  ]
+}
